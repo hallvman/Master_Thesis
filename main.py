42527c8045857a3d63044eb199318466c8186808
hallvman <Hallv-a@otmail.bo>
2023-04-25 11:56:48 +0200

    lagt til filer

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+
+from DbConnector import DbConnector
+from insert_data.DatabaseSetup import DatabaseSetup
+
+def connect():
+    # Creates a connection with the database
+    connector = DbConnector()
+    # Creates a database setup object
+    setup = DatabaseSetup(connector)
+    # Drops the table if it is already created
+    setup.drop_tables()
+    # Creates the tables if they don't already exist
+    setup.create_tables()
+    # Insert user.
+    setup.insert_users()
+    # Inserts activities and track points
+    setup.traverse_dataset()
+
+def main():
+    connect()
+
+
+if __name__ == "__main__":
+    main()
