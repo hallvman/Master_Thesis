a5059f28bff492bc48d049cca8276c736560c42b
hallvman <Hallv-a@otmail.bo>
2023-04-26 12:32:13 +0200

    Test

--- insert_data/DatabaseSetup.py
+++ insert_data/DatabaseSetup.py
@@ -5,7 +5,7 @@ import uuid
 from tabulate import tabulate
 
 from DbConnector import DbConnector
-from insert_data.Activity import Activity
+from task_1.Activity import Activity
 
 """
 Handles the database setup.
@@ -14,7 +14,9 @@ Handles the database setup.
 - Prints data from the database
 """
 
+
 class DatabaseSetup:
+
     def __init__(self, connection: DbConnector):
         self.connection = connection
         self.db_connection = self.connection.db_connection
@@ -22,53 +24,52 @@ class DatabaseSetup:
         self.labels_dict = {}
 
     def create_user_table(self):
-        try:
-            print("\n---- Trying to create User Table ----")
-            query = """CREATE TABLE IF NOT EXISTS USER (
+        query = """CREATE TABLE IF NOT EXISTS USER (
                    id VARCHAR(50) NOT NULL PRIMARY KEY,
                    has_labels BOOLEAN);
                 """
-            self.cursor.execute(query)
-            self.db_connection.commit()
-            print("\n---- User Table Created----\n")
-        except Exception as e:
-            print("User Table not created: ", e)
+        self.cursor.execute(query)
+        self.db_connection.commit()
 
     def create_activity_table(self):
-        try:
-            print("\n---- Trying to create Activity Table ----")
-            query = """CREATE TABLE IF NOT EXISTS ACTIVITY (
+        query = """CREATE TABLE IF NOT EXISTS ACTIVITY (
                    id varchar(128) NOT NULL PRIMARY KEY,
                    user_id VARCHAR(50) NOT NULL,
-                   FOREIGN KEY (user_id) REFERENCES Master_Thesis_DB.USER(id),
+                   FOREIGN KEY (user_id) REFERENCES test_db.USER(id),
                    transportation_mode VARCHAR(30), 
                    start_date_time DATETIME,
                    end_date_time DATETIME);
                 """
-            self.cursor.execute(query)
-            self.db_connection.commit()
-            print("\n---- Activity Table Created ----\n")
-        except Exception as e:
-            print("Activity Table not created: ", e)
+        self.cursor.execute(query)
+        self.db_connection.commit()
 
     def create_track_point_table(self):
-        try:
-            print("\n---- Trying to create Tack Points Table ----")
-            query = """CREATE TABLE IF NOT EXISTS TRACK_POINT (
+        query = """CREATE TABLE IF NOT EXISTS TRACK_POINT (
                    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
                    activity_id varchar(128) NOT NULL,
-                   FOREIGN KEY (activity_id) REFERENCES Master_Thesis_DB.ACTIVITY(id),
+                   FOREIGN KEY (activity_id) REFERENCES test_db.ACTIVITY(id),
                    lat DOUBLE,
                    lon DOUBLE, 
                    altitude INT,
                    data_days DOUBLE, 
                    data_time DATETIME);
                 """
-            self.cursor.execute(query)
-            self.db_connection.commit()
-            print("\n---- Tack Points Table Created ----\n")
-        except Exception as e:
-            print("Track Points Table not created:", e)
+        self.cursor.execute(query)
+        self.db_connection.commit()
+
+    def print_users(self):
+        query = "SELECT * FROM test_db.USER"
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        # Using tabulate to show the table in a nice way
+        print(f"Data from table USERS tabulated:\n{tabulate(rows, headers=self.cursor.column_names)}")
+
+    def print_activity(self):
+        query = "SELECT * FROM test_db.ACTIVITY"
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        # Using tabulate to show the table in a nice way
+        print(f"Data from table USERS tabulated:\n{tabulate(rows, headers=self.cursor.column_names)}")
 
     def create_tables(self):
         self.create_user_table()
@@ -79,17 +80,22 @@ class DatabaseSetup:
         print('Are you sure you would like to drop the tables? (Y/N)')
         decision = input()
         if decision == "Y" or decision == "y":
-            query = """DROP TABLE Master_Thesis_DB.USER, Master_Thesis_DB.ACTIVITY, Master_Thesis_DB.TRACK_POINT CASCADE """
+            query = """DROP TABLE test_db.USER, test_db.ACTIVITY, test_db.TRACK_POINT CASCADE """
             self.cursor.execute(query)
-            print('\n---- Tables dropped ----\n')
+            print('Tables dropped')
         else:
             print('No tables were dropped')
+
+    def show_tables(self):
+        self.cursor.execute("SHOW TABLES")
+        rows = self.cursor.fetchall()
+        print(tabulate(rows, headers=self.cursor.column_names))
 
     def is_plt_file(self, extension):
         return extension == ".plt"
 
     def get_extension(self, path):
-        extension = os.path.splitext(path)
+        name, extension = os.path.splitext(path)
         return extension
 
     def get_nr_of_lines(self, path):
@@ -110,7 +116,7 @@ class DatabaseSetup:
         @return: A list of labels
         @rtype: list
         """
-        label_path = "dataset/labeled_ids.txt"
+        label_path = "dataset/dataset/labeled_ids.txt"
         labels = open(label_path, 'r').read().splitlines()
         return labels
 
@@ -120,7 +126,7 @@ class DatabaseSetup:
         @return: A sorted list of user ids
         @rtype: list
         """
-        path = "dataset/Data"
+        path = "dataset/dataset/Data"
         user_ids = sorted([f for f in os.listdir(path) if not f.startswith('.')])
         return user_ids
 
@@ -134,15 +140,13 @@ class DatabaseSetup:
         user_ids = self.get_user_ids()
 
         try:
-            print("\n---- Trying to insert Users in USER-table ----")
             for user in user_ids:
                 has_label = False
                 if user in user_labels:
                     has_label = True
-                query = "INSERT INTO Master_Thesis_DB.USER (id, has_labels) VALUES ('%s', %s)"
+                query = "INSERT INTO test_db.USER (id, has_labels) VALUES ('%s', %s)"
                 self.cursor.execute(query % (user, has_label))
             self.db_connection.commit()
-            print("\n---- Users Inserted in table: USER ----\n")
         except Exception as e:
             print(f'An error occurred while inserting users:{sys.exc_info()[2]}')
 
@@ -249,7 +253,7 @@ class DatabaseSetup:
         @return: label_activity_list - a list with all label activities
         @rtype: list
         """
-        for root, dirs, files in os.walk('dataset/Data', topdown=True):
+        for root, dirs, files in os.walk('dataset/dataset/Data', topdown=True):
             for file in files:
                 if file == "labels.txt":
                     with open(os.path.join(root, file)) as f:
@@ -275,7 +279,7 @@ class DatabaseSetup:
 
         # populate label_dict
         self.create_label_activities()
-        for root, dirs, files in os.walk('dataset/Data', topdown=True):
+        for root, dirs, files in os.walk('dataset/dataset/Data', topdown=True):
             if len(dirs) == 0 and len(files) > 0:
                 for file in files:
                     path = os.path.join(root, file)  # The current path
@@ -303,12 +307,12 @@ class DatabaseSetup:
         @rtype: None
         """
         if activity.transportation_mode is None:
-            query = """INSERT INTO Master_Thesis_DB.ACTIVITY (id, user_id, start_date_time, end_date_time) 
+            query = """INSERT INTO test_db.ACTIVITY (id, user_id, start_date_time, end_date_time) 
                                 VALUES ('%s', '%s','%s','%s')"""
             self.cursor.execute(query % (
                 activity.id, activity.user_id, activity.start_date_time, activity.end_date_time))
         else:
-            query = """INSERT INTO Master_Thesis_DB.ACTIVITY (id, user_id, transportation_mode, start_date_time, end_date_time) 
+            query = """INSERT INTO test_db.ACTIVITY (id, user_id, transportation_mode, start_date_time, end_date_time) 
                                                 VALUES ('%s', '%s','%s', '%s', '%s')"""
             self.cursor.execute(query % (
                 activity.id, activity.user_id, activity.transportation_mode, activity.start_date_time,
@@ -325,7 +329,7 @@ class DatabaseSetup:
         @rtype: None
         """
 
-        trajectory_query = """INSERT INTO Master_Thesis_DB.TRACK_POINT (activity_id, lat, lon, altitude, data_days, data_time) 
+        trajectory_query = """INSERT INTO test_db.TRACK_POINT (activity_id, lat, lon, altitude, data_days, data_time) 
                                   VALUES (%s, %s, %s, %s, %s, %s)"""
         self.cursor.executemany(trajectory_query, track_points)
         self.db_connection.commit()
@@ -339,7 +343,7 @@ class DatabaseSetup:
         @rtype: None
         """
 
-        users_query = """INSERT INTO Master_Thesis_DB.TRACK_POINT (id, has_label) 
+        users_query = """INSERT INTO test_db.TRACK_POINT (id, has_label) 
                                   VALUES (%s, %s)"""
         self.cursor.executemany(users_query, users)
         self.db_connection.commit()
